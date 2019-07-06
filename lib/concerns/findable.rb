e9322037ab0fc446f074ea559beb1f2acfa99364
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-06 00:34:48 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -6,7 +6,7 @@ module Findable
   module ClassMethods
 
     def self.find_by_name(name)
-      @@artists.detect{|a| a.name == name}
+      self.detect{|a| a.name == name}
     end
 
     def self.find_by_name(name)
