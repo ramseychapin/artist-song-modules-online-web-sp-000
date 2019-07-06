6579c46982b3e9b405e047f363a6a59ad176685a
Ramsey Chapin <ramsey@ramseychapin.com>
2019-07-06 00:37:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
-  
+
   attr_accessor :name
   attr_reader :songs
 
@@ -15,10 +15,6 @@ class Artist
     @@artists << self
     @songs = []
   end
-
-  # def self.find_by_name(name)
-  #   @@artists.detect{|a| a.name == name}
-  # end
 
   def self.all
     @@artists
